ab9a62d3fb4c23b57596b4bafc08da3eda82336d
boppe123 <44640763+boppe123@users.noreply.github.com>
2018-11-02 12:52:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/webhook", function(req, res) {
+
+	let text = 'tester';
+	
+return res.json({ 'fulfillmentText': `just a test!` });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
